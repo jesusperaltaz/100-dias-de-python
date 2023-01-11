6c50dcc80587b8788914d1c9ce302c8d0df8d819
jesusperaltaz <jesus.peralta.entr@gmail.com>
2023-01-11 15:21:09 -0600

    solución día 11

--- retos/20-dias/dia-11.py
+++ retos/20-dias/dia-11.py
@@ -4,3 +4,11 @@
 Intercambia los valores de dos varibles e imprime  
         de ambas e imprime el resultado en una sola línea
 """
+v1=10
+v2=20
+print("numero 1:", v1)
+print("numero 2:", v2)
+vguardado=v1
+v1=v2
+v2=vguardado
+print("numero 1:",v1,"numero2:",v2)
