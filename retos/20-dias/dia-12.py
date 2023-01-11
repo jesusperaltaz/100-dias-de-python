55836d393b5b936953ef275f1c52e567b337bee2
jesusperaltaz <jesus.peralta.entr@gmail.com>
2023-01-11 15:30:52 -0600

    solución día 12

--- retos/20-dias/dia-12.py
+++ retos/20-dias/dia-12.py
@@ -4,3 +4,9 @@
 Intercambia los valores de dos variables e imprime su ubicación en memoria
                         utilizando la función id()
 """
+a,b=100,200
+print("a=",a,id(a))
+print("b=",b,id(b))
+a,b=b,a
+print("a=",a,id(a))
+print("b=",b,id(b))
